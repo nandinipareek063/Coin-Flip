5b81a629eda46d3de577ce1ab27cd66311c9d6ed
nandinipareek063 <nandinipareek063@gmail.com>
2020-07-07 12:06:33 +0530

    Added Flipping a coin is added

--- coinFlip.sh
+++ coinFlip.sh
@@ -1,1 +1,14 @@
+
 echo "Welcome to Flip Coin Simulation Problem"
+
+#UC1
+echo "As a Simulator start with Flipping a Coin to Display Heads or Tails as winner - Use ((RANDOM)) to find Heads or Tails"
+heads=0
+tails=0
+no=$((RANDOM%2))
+if [ $no -eq 1 ]
+then
+	echo "Heads win "
+else
+        echo "Tails win"
+fi
